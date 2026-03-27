ef23149645ab1398a44dd1785906313d5f9ee07d
Steven Tompkins <steven.tompkins.jr@gmail.com>
2026-03-26 22:17:13 -0500

    art: vanilla-style GUI texture for RecipeMemoryBoxScreen

--- neoforge/src/main/java/io/github/scuba10steve/s3/advanced/gui/client/RecipeMemoryBoxScreen.java
+++ neoforge/src/main/java/io/github/scuba10steve/s3/advanced/gui/client/RecipeMemoryBoxScreen.java
@@ -1,12 +1,17 @@
 package io.github.scuba10steve.s3.advanced.gui.client;
 
+import io.github.scuba10steve.s3.advanced.StevesAdvancedStorage;
 import io.github.scuba10steve.s3.advanced.gui.server.RecipeMemoryBoxMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class RecipeMemoryBoxScreen extends AbstractContainerScreen<RecipeMemoryBoxMenu> {
+
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(
+        StevesAdvancedStorage.MOD_ID, "textures/gui/recipe_memory_box.png");
 
     public RecipeMemoryBoxScreen(RecipeMemoryBoxMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -18,36 +23,7 @@ public class RecipeMemoryBoxScreen extends AbstractContainerScreen<RecipeMemoryB
     protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
-        // Draw a standard inventory-style background rectangle
-        graphics.fill(x, y, x + imageWidth, y + imageHeight, 0xFFC6C6C6);
-        graphics.fill(x + 1, y + 1, x + imageWidth - 1, y + imageHeight - 1, 0xFF8B8B8B);
-
-        // Draw slot backgrounds for the 9 pattern slots (3×3 grid)
-        for (int i = 0; i < RecipeMemoryBoxMenu.PATTERN_SLOT_COUNT; i++) {
-            int row = i / 3;
-            int col = i % 3;
-            int slotX = x + 25 + col * 18;
-            int slotY = y + 16 + row * 18;
-            graphics.fill(slotX, slotY, slotX + 18, slotY + 18, 0xFF373737);
-            graphics.fill(slotX + 1, slotY + 1, slotX + 17, slotY + 17, 0xFF8B8B8B);
-        }
-
-        // Player inventory (3 rows × 9 cols)
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 9; col++) {
-                int slotX = x + 7 + col * 18;
-                int slotY = y + 83 + row * 18;
-                graphics.fill(slotX, slotY, slotX + 18, slotY + 18, 0xFF373737);
-                graphics.fill(slotX + 1, slotY + 1, slotX + 17, slotY + 17, 0xFF8B8B8B);
-            }
-        }
-        // Hotbar (9 slots)
-        for (int col = 0; col < 9; col++) {
-            int slotX = x + 7 + col * 18;
-            int slotY = y + 141;
-            graphics.fill(slotX, slotY, slotX + 18, slotY + 18, 0xFF373737);
-            graphics.fill(slotX + 1, slotY + 1, slotX + 17, slotY + 17, 0xFF8B8B8B);
-        }
+        graphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
     }
 
     @Override
